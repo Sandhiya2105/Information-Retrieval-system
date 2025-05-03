f4c14422943ade96e9c693eea7102334b25cf092
Sandhiya <sandhiya2116195@gmail.com>
2025-05-03 21:37:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project',
+    version= '0.0.0',
+    author= 'Sandhiya',
+    author_email='sandhiya2116195@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
